9b42342836c9e5445975ab0b4095c9e9c70f242f
mateus <ffrmateus@gmail.com>
2021-12-16 13:11:17 -0300

    Conhecendo JSX

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom'
+
+
+const tag = <strong>Olá React!</strong>
+
+
+ReactDOM.render(
+    <div>
+        {tag}
+    </div>
+    , document.getElementById('root')
+    )
