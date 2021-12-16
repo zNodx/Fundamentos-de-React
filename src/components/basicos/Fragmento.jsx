2bffba4f13ba7ee2ee779074068e356e2f2625b5
mateus <ffrmateus@gmail.com>
2021-12-16 14:24:12 -0300

    React Fragment

---
+++ src/components/basicos/Fragmento.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Fragmento(props){
+
+    return( 
+      
+        <> 
+            <h2>Fragmento</h2>
+            <p>Cuidado com esse erro!</p>
+        </>
+
+    )
+
+}
--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
-
+import Fragmento from './components/basicos/Fragmento'
 
 
 
 ReactDOM.render(
-    <div>
+    <div id="app">
         <Primeiro></Primeiro>
         <ComParametro 
         titulo="Situação do Aluno" 
         aluno="Pedro"
         nota={6.9}
         ></ComParametro>
+        <Fragmento/>
     </div>
     , document.getElementById('root')
     )
